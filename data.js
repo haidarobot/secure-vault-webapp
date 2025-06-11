ae269e7972cd29e50b7344c0f39fa757278b1349
haidarobot <haidarasliok@gmail.com>
2025-06-11 16:32:41 +0700

    first commit

---
+++ data.js
@@ -0,0 +1,26 @@
+// data.js
+// Tempat menyimpan semua data kredensial.
+// Edit array ini untuk mengelola password Anda.
+
+const passwordEntries = [
+  { 
+    site: "Google", 
+    username: "haidar.rasyid@email.com", 
+    password: "PasswordGoogleSuperRahasia_123" 
+  },
+  { 
+    site: "GitHub", 
+    username: "haidar-dev", 
+    password: "PasswordGitHubAmanBanget!@#" 
+  },
+  { 
+    site: "Bank Digital", 
+    username: "081234567890", 
+    password: "PINRahasia456"
+  },
+  {
+    site: "LMS Kampus",
+    username: "2201234567",
+    password: "IniPasswordLMS"
+  }
+];
